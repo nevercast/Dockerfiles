2bbf023191aea77dcdc2fd2fcd7a5e5e259ebc5b
Josh Lloyd <j.nevercast@gmail.com>
2015-06-23 17:47:55 +1200

    Tidy up build.py to be considered for #2

--- build.py
+++ build.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python
-from sys import argv as args
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from sys import argv
 from os import listdir as ls
 from os.path import join, isfile
 from subprocess import call
 from time import time
 begin = time()
+
 # Select developer name
-devName = (len(args) > 1 and args[1]) or 'nevercast'
+
+developer_name = len(argv) > 1 and argv[1] or 'nevercast'
+
 # Iterate one depth of folders looking for Dockerfiles
+
 for file in ls('.'):
-	if file[0] == '.' or isfile(file):
-		continue
-	if isfile(join(file, 'Dockerfile')):
-		imageName = '{}/{}:latest'.format(devName, file)
-		print 'Building ' + imageName
-		call(['docker','build','-t',imageName, file])
-print 'Completed build in', (time() - begin), 'seconds'
+    if file[0] == '.' or isfile(file):
+        continue
+    if isfile(join(file, 'Dockerfile')):
+        image_name = '{}/{}:latest'.format(developer_name, file)
+        print('Building', image_name)
+        call(['docker', 'build', '-t', image_name, file])
+
+# Print operation completion message
+
+print('Completed build in', time() - begin, 'seconds')
